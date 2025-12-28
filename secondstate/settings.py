653fbcc92ab2f8f79bf468fcae70f3dc9aacf0a5
Jeremy Klein <bekahsherry@gmail.com>
2025-12-28 12:33:47 -0800

    initial commit

--- secondstate/settings.py
+++ secondstate/settings.py
@@ -7,7 +7,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-insecure-change-me")
 # DEBUG = True
 DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
