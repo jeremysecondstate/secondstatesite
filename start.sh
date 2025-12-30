fb0de46cbdd6a4ec0ea60848e98140e298b17b45
Jeremy Klein <bekahsherry@gmail.com>
2025-12-29 23:22:36 -0800

    new stuff commit

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ set -o errexit
 
 # Render disk is mounted at runtime, so /var/data is writable here
 mkdir -p /var/data/media/artworks
+mkdir -p /var/data/sales
 
 python manage.py migrate --noinput
 
