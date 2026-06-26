6a4e5c110d245434404455ca998359b26fbf5f8b
jeremysecondstate <jeremy@secondstate.art>
2026-06-26 11:45:02 -0700

    Use detailed description error launcher

--- catalogapp/catalogapp_inv.py
+++ catalogapp/catalogapp_inv.py
@@ -1,7 +1,7 @@
 try:
-    from catalogapp.catalogapp_inv_ui import main
+    from catalogapp.catalogapp_inv_ui_detail import main
 except ImportError:
-    from catalogapp_inv_ui import main
+    from catalogapp_inv_ui_detail import main
 
 
 if __name__ == "__main__":
