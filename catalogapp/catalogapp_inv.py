ed1cf0f98f9bd1a09e3450ef9cdea74d30ca26f7
jeremysecondstate <jeremy@secondstate.art>
2026-06-26 22:08:59 -0700

    Use catalog UI with header row handling

--- catalogapp/catalogapp_inv.py
+++ catalogapp/catalogapp_inv.py
@@ -1,8 +1,7 @@
 try:
-    from catalogapp.catalogapp_inv_ui_delete import main
+    from catalogapp.catalogapp_inv_ui_clean import main
 except ImportError:
-    from catalogapp_inv_ui_delete import main
-
+    from catalogapp_inv_ui_clean import main
 
 if __name__ == "__main__":
     main()
